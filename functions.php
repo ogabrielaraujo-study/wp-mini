0824d3ba27f51710c61e949259fd8a31aecb6cb8
Gabriel Araujo <gabriiel66@gmail.com>
2020-02-16 15:57:46 -0300

    basics functions

--- functions.php
+++ functions.php
@@ -5,3 +5,25 @@
  * Author: Gabriel Araujo
  * Author URL: https://ogabrielaraujo.com.br
  */
+
+// Sets content width.
+if (!isset($content_width)) {
+	$content_width = 1200;
+}
+
+// Theme Support
+function theme_support_setup() {
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
+	add_theme_support('custom-background');
+	add_theme_support('custom-logo');
+	add_theme_support('automatic-feed-links');
+	add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption']);
+}
+add_action('after_setup_theme', 'theme_support_setup');
+
+// Loads style.css
+function theme_stylesheet() {
+	wp_enqueue_style('style-mini', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'theme_stylesheet');
--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
